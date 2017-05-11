c4e77d62588b1b24b02d1933a8a288e4347451bf
SPECTER\Luis David <luisdcb97@gmail.com>
2017-05-12 00:31:16 +0100

    Bar update
    Adiciona o bar e o popup de baguetes
    Muda os headers para sections

--- Principal/JavaScript/Inicio.js
+++ Principal/JavaScript/Inicio.js
@@ -3,9 +3,37 @@
 let debug = true;
 let debug_prefix = "[DEBUG]\t";
 
+let timeoutTextIds=[];
+
 let definicoes = {
     volume: 0.25,
     muted: false
+};
+
+let jogador = {
+    dinheiro: 50,
+    energia: 100,
+    alteraDinheiro: function (valor) {
+        if (this.dinheiro + valor < 0){
+            return false;
+        }
+        this.dinheiro += valor;
+        return true;
+    },
+    alteraEnergia: function (valor) {
+        if (this.energia == 0 || this.energia == 100){
+            return false;
+        }
+
+        this.dinheiro += valor;
+        if (this.energia < 0 ){
+            this.energia = 0;
+        }
+        else if(this.energia > 100){
+            this.energia = 100;
+        }
+        return true;
+    }
 };
 
 (function () {
@@ -13,17 +41,27 @@ let definicoes = {
 }());
 
 function main() {
+    let botaoJogar = document.getElementById("BotaoJogar");
     let botaoAjuda = document.getElementById("BotaoAjuda");
     let botaoOpcoes = document.getElementById("BotaoOpcoes");
     let botaoSair = document.getElementById("BotaoSair");
     let botaoVoltarAjuda = document.getElementById("BotaoVoltarAjuda");
     let botaoVoltarOpcoes = document.getElementById("BotaoVoltarOpcoes");
 
+    botaoJogar.addEventListener("click", function (event) {
+        changeSection(event, document.getElementById("Bar"));
+        botaoVoltarAjuda.removeEventListener("click", goBackMain);
+        botaoVoltarOpcoes.removeEventListener("click", goBackMain);
+
+        botaoVoltarAjuda.addEventListener("click", goBackBar);
+        botaoVoltarOpcoes.addEventListener("click", goBackBar);
+    });
+
     botaoAjuda.addEventListener("click", function (event) {
-        changeHeader(event, document.getElementById("Ajuda"))
+        changeSection(event, document.getElementById("Ajuda"))
     });
     botaoOpcoes.addEventListener("click", function (event) {
-        changeHeader(event, document.getElementById("Opcoes"))
+        changeSection(event, document.getElementById("Opcoes"))
     });
     botaoVoltarAjuda.addEventListener("click", goBackMain);
     botaoVoltarOpcoes.addEventListener("click", goBackMain);
@@ -31,6 +69,42 @@ function main() {
         window.close();
     });
     addListenersOpcoes();
+    addListenersBar();
+}
+
+function addListenersBar() {
+    let botaoAjuda= document.getElementById("BotaoInterrogacaoBar");
+    let botaoOpcoes = document.getElementById("BotaoSomBar");
+    let botaoSair = document.getElementById("BotaoSairBar");
+    let senhora = document.getElementById("BotaoSenhoraBar");
+
+
+    botaoAjuda.addEventListener("click", function(event){
+        changeSection(event, document.getElementById("Ajuda"));
+    });
+
+    botaoOpcoes.addEventListener("click", function(event){
+        changeSection(event, document.getElementById("Opcoes"));
+    });
+
+    botaoSair.addEventListener("click", function (event) {
+        window.close();
+    });
+
+    senhora.addEventListener("click", showBaguetes);
+
+    let botaoFecharBaguetes = document.getElementById("FecharBaguetes");
+
+    botaoFecharBaguetes.addEventListener("click", hideBaguetes);
+
+    let bagueteQueijo = document.getElementById("Queijo");
+    bagueteQueijo.addEventListener("click", function (event) {
+        let alterado = compraBaguete(3, 30);
+        if(alterado){
+            alteraDadosJogador();
+        }
+    });
+    alteraDadosJogador();
 }
 
 function addListenersOpcoes(){
@@ -103,9 +177,9 @@ function addListenersOpcoes(){
 
 }
 
-function getVisibleHeader(){
-    let headers = document.getElementsByTagName("header");
-    for (let head of headers) {
+function getVisibleSection(){
+    let sections = document.getElementsByTagName("section");
+    for (let head of sections) {
         if(head.style.display !== "none"){
             return head;
         }
@@ -113,18 +187,26 @@ function getVisibleHeader(){
     return null;
 }
 
-function changeHeader(event, header) {
-    let current = getVisibleHeader();
+function changeSection(event, section) {
+    let current = getVisibleSection();
     current.style.display = "none";
-    header.style.display = "block";
-
+    section.style.display = "block";
+    return current;
 }
 
 function goBackMain(event) {
-    let current = getVisibleHeader();
-    let header = document.getElementById("MenuPrincipal");
+    let current = getVisibleSection();
+    let section = document.getElementById("MenuPrincipal");
 
-    header.style.display = "block";
+    section.style.display = "block";
+    current.style.display = "none";
+}
+
+function goBackBar(event) {
+    let current = getVisibleSection();
+    let section = document.getElementById("Bar");
+
+    section.style.display = "block";
     current.style.display = "none";
 }
 
@@ -148,4 +230,95 @@ function changeVolume(change){
     for (let audio of document.getElementsByTagName("audio")) {
         audio.volume = definicoes.volume;
     }
+}
+
+function compraBaguete(preco, energia){
+    for (let id1 of timeoutTextIds){
+        clearTimeout(id1);
+    }
+    timeoutTextIds = [];
+
+    let compra;
+    let visor = document.getElementById("Visor");
+    if (!jogador.alteraDinheiro(-preco)) {
+        alteraTexto(visor, "Não tem saldo suficiente!", 10);
+        compra = false;
+    }
+    else{
+        jogador.alteraEnergia(energia);
+        alteraTexto(visor, "Compra efetuada!", 10);
+        compra = true;
+    }
+
+    let id = setTimeout(function () {
+        alteraTexto(visor, "Efetue a sua compra.");
+    }, 1500);
+    timeoutTextIds.push(id);
+    return compra;
+}
+
+function alteraTexto(par, subst, timestep = 25) {
+    let time = 0;
+    for( let i = par.innerHTML.length-1; i > 0; i--){
+        let id = setTimeout(function () {
+            par.innerHTML = par.innerHTML.slice(0, -1);
+        }, time);
+        timeoutTextIds.push(id);
+        time += timestep;
+    }
+
+    let id = setTimeout(function () {
+        par.innerHTML = subst.charAt(0);
+    }, time);
+    timeoutTextIds.push(id);
+    time += timestep;
+
+    for( let j = 1; j < subst.length; j++){
+        let char = subst.charAt(j);
+        let id = setTimeout(function () {
+            par.innerHTML = par.innerHTML + char;
+        }, time);
+        timeoutTextIds.push(id);
+        time+= timestep;
+    }
+}
+
+function alteraDadosJogador() {
+    let saldo = document.getElementById("DinheiroValor");
+    let energia = document.getElementById("Energia");
+
+    saldo.innerHTML = jogador.dinheiro;
+    // TODO mudar o icone da barra de energia consoante a energia do jogador
+    if (jogador.energia === 100){
+
+    }
+    else if(jogador.energia >= 75){
+
+    }
+    else if(jogador.energia >= 50){
+
+    }
+    else if(jogador.energia >= 25){
+
+    }
+    else if(jogador.energia >= 0){
+
+    }
+}
+
+function showBaguetes(event){
+    let baguetes = document.getElementById("CompraBaguetes");
+    baguetes.style.display = "block";
+    let visor = document.getElementById("Visor");
+    visor.innerHTML = "Efetua a sua compra."
+}
+
+function hideBaguetes(event){
+    for (let id1 of timeoutTextIds){
+        clearTimeout(id1);
+    }
+    timeoutTextIds = [];
+
+    let baguetes = document.getElementById("CompraBaguetes");
+    baguetes.style.display = "none";
 }
