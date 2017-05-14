35049b96900582bdd01a118a84ca6f396d987edf
fgbento <franciscogranjabento@gmail.com>
2017-05-14 20:30:12 +0100

    geraçao de mapa1 TC perfeito, falta "ganhar"

--- Minijogos/Ano 1/Semestre 2/TC/Javascript/Cabo.js
+++ Minijogos/Ano 1/Semestre 2/TC/Javascript/Cabo.js
@@ -1,24 +1,35 @@
 "use strict";
 
 class Cabo{
-    constructor(x,y,rotacao,entrada,saida){
+    constructor(x,y,entrada,saida){
         this.x = x;
         this.y = y;
-        this.rotacao = rotacao;
         this.entrada = entrada;
         this.saida = saida;
     }
-
-    desenha(contexto) {
-        contexto.save();
-
-        contexto.fillStyle = this.fillColor;
-        contexto.strokeStyle = this.strokeColor;
-        contexto.beginPath();
-        contexto.arc(this.x, this.y, this.raio, 0, 2*Math.PI);
-        contexto.fill();
-        contexto.stroke();
-
-        contexto.restore();
+    //       2
+    //    0     1
+    //       3
+    alteraRotacao(entrada,saida){
+        if (this.entrada == 0 && this.saida == 1){
+            this.entrada = 2;
+            this.saida = 3;
+        }
+        else if (this.entrada == 2 && this.saida == 3){
+            this.entrada = 0;
+            this.saida = 1;
+        }
+        else if (this.entrada == 0 && this.saida == 2){
+            this.entrada = 2;
+            this.saida = 1;
+        }
+        else if (this.entrada == 2 && this.saida == 1){
+            this.entrada = 1;
+            this.saida = 3;
+        }
+        else if (this.entrada == 1 && this.saida == 3){
+            this.entrada = 3;
+            this.saida = 0;
+        }
     }
 }
--- Minijogos/Ano 1/Semestre 2/TC/Javascript/TCJogo.js
+++ Minijogos/Ano 1/Semestre 2/TC/Javascript/TCJogo.js
@@ -1,45 +1,192 @@
 "use strict";
 
 
-let game = new Phaser.Game(800,540,Phaser.AUTO, 'TC');
-let xs = [];
-let i;
 
-let gameState = {
+let i;
+let x, y;
+
+var gameState = {
     preload: function () {
-        game.load.image('fundo', "../../../../../Principal/Recursos/arduinoFundo.jpg");
-        game.load.image('cabo1', "../../../../../Principal/Recursos/caboArduinoAmarelo.png");
-        game.load.image('cabo2', "../../../../../Principal/Recursos/caboArduinoAzul.png");
-        game.load.image('cabo3', "../../../../../Principal/Recursos/caboArduinoVermelho.png");
-        game.load.image('cabo4', "../../../../../Principal/Recursos/caboArduinoAmarelo90Graus.png");
-        game.load.image('cabo5', "../../../../../Principal/Recursos/caboArduinoAzul90Graus.png");
-        game.load.image('cabo6', "../../../../../Principal/Recursos/caboArduinoVermelho90Graus.png");
-        game.load.image('inicial', "../../../../../Principal/Recursos/caboArduinoInicial.png");
-        game.load.image('final', "../../../../../Principal/Recursos/caboArduinoFinal.png");
-        game.load.image('ledApagado', "../../../../../Principal/Recursos/ledArduino.png");
-        game.load.image('ledLigado', "../../../../../Principal/Recursos/ledArduinoLigado.png");
+        this.load.image('fundo', "../../../../../Principal/Recursos/arduinoFundo.jpg");
+        this.load.image('cabo1', "../../../../../Principal/Recursos/caboArduinoAmarelo.png");
+        this.load.image('cabo2', "../../../../../Principal/Recursos/caboArduinoAzul.png");
+        this.load.image('cabo3', "../../../../../Principal/Recursos/caboArduinoVermelho.png");
+        this.load.image('cabo4', "../../../../../Principal/Recursos/caboArduinoAmarelo90Graus.png");
+        this.load.image('cabo5', "../../../../../Principal/Recursos/caboArduinoAzul90Graus.png");
+        this.load.image('cabo6', "../../../../../Principal/Recursos/caboArduinoVermelho90Graus.png");
+        this.load.image('inicial', "../../../../../Principal/Recursos/caboArduinoInicial.png");
+        this.load.image('final', "../../../../../Principal/Recursos/caboArduinoFinal.png");
+        this.load.image('ledApagado', "../../../../../Principal/Recursos/ledArduino.png");
+        this.load.image('ledLigado', "../../../../../Principal/Recursos/ledArduinoLigado.png");
 
     },
     create: function () {
-        var s = game.add.sprite(0, 0, 'fundo');
-        var cab = game.add.sprite(100, 98, 'inicial');
-        var cab = game.add.sprite(100+9*54, 250, 'final');
-        for (let i = 0; i < 8; i++) {
-            var cab = game.add.sprite(154+i*54, 98, 'cabo'+Math.floor(Math.random()*5+1));
+        this.spriteCabos = [];
+        this.cabos = [];
+        this.background = this.add.sprite(0, 0, 'fundo');
+        this.caboIni = this.add.sprite(100, 98, 'inicial');
+        this.caboFin = this.add.sprite(100+9*54, 250, 'final');
+        for (let i = 0; i < 1; i++) {
+            x = 154+i*54+27;
+            y = 98+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 154+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+        }
+        for (let i = 1; i < 2; i++) {
+            x = 100+i*54+27;
+            y = 152+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+        }
+        for (let i = 1; i < 2; i++) {
+            x = 100+i*54+27;
+            y = 196+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
         }
         for (let i = 1; i < 9; i++) {
-            var cab = game.add.sprite(100+i*54, 152, 'cabo'+Math.floor(Math.random()*5+1));
+            x = 100+i*54+27;
+            y = 250+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,2));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*3+4)));
+            i++;
+            x = 100+i*54+27;
+            this.cabos.push(new Cabo(x,y,0,1));
+            this.spriteCabos.push(this.add.sprite(x, y, 'cabo'+Math.floor(Math.random()*2+1)));
         }
-        for (let i = 1; i < 9; i++) {
-            var cab = game.add.sprite(100+i*54, 196, 'cabo'+Math.floor(Math.random()*5+1));
+        this.led = this.add.sprite(329+19,356,'ledApagado');
+        //if pressed
+        for(let i = 0; i< this.spriteCabos.length; i++){
+            this.spriteCabos[i].anchor.setTo(0.5,0.5);
+            this.spriteCabos[i].inputEnabled = true;
+            this.spriteCabos[i].events.onInputDown.add(this.roda,this.spriteCabos[i]);
+            this.spriteCabos[i].events.onInputDown.add(this.rodaCabo,this.cabos[i]);
+
         }
-        for (let i = 1; i < 9; i++) {
-            var cab = game.add.sprite(100+i*54, 250, 'cabo'+Math.floor(Math.random()*5+1));
-        }
-        var led = game.add.sprite(329,356,'ledApagado');
+
+
+    },
+    rodaCabo: function () {
+        this.alteraRotacao(this.entrada,this.saida);
+    },
+    roda: function () {
+        this.angle += 90;
+    },
+    update: function(){
+        //if pressed
+        
     }
 };
 
+var game = new Phaser.Game(800,540,Phaser.AUTO, 'TC');
 
 game.state.add("MainGame", gameState);
 game.state.start("MainGame");
