17150fe1a090d93b5a5dc8534a63bc0cd4dcd21c
inesilva <ines_ngc@hotmail.com>
2017-05-15 17:49:44 +0100

    pontos a funcionar no jogo de AMI

--- Principal/JavaScript/jogoAMI.js
+++ Principal/JavaScript/jogoAMI.js
@@ -1,17 +1,23 @@
 "use strict";
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "Nome");
-var fundo;
-var mesa;
+var fundo,mesa,integral;
 var head;
 var sprite1;
-var timer, timerEvent, text;
+var timer, timerEvent, text, mensagem;
+var pontos,pontuacao=0;
+var  mesas[],funcoes[];
+
 
 let mainState = {
    preload: function () {
        game.load.image('fundo','../Recursos/fundo_jogoAMI.png');
        game.load.image('mesa','../Recursos/mesa1.png');
        game.load.image('head','../Recursos/topviewhead1.png');
+       game.load.image('integral', '../Recursos/integral.png');
+       game.debug.text("Pontos: ",500,20,16,"#ffffff");
+       game.load.image('mensagem', '../Recursos/ops..perdeu.png');
+
    },
 
    create: function () {
@@ -20,44 +26,52 @@ let mainState = {
 
        game.physics.startSystem(Phaser.Physics.ARCADE);
 
+       gerarMesas();
        fundo = game.add.tileSprite(0,0,800,600,'fundo');
        mesa = game.add.sprite(300,40, 'mesa');
        sprite1 = game.add.sprite(50,200,'head');
+       integral = game.add.sprite(400,300,'integral');
+
 
        game.physics.enable(sprite1, Phaser.Physics.ARCADE);
        sprite1.body.collideWorldBounds=true; //para o boneco não sair do ecrã
-       //sprite1.body.checkCollision.up = false;
-       //sprite1.body.checkCollision.down = false;
-       //sprite1.body.immovable = true;
 
        game.physics.enable(mesa, Phaser.Physics.ARCADE);
+       mesa.body.velocity.x = -90;
 
+       game.physics.enable(integral, Phaser.Physics.ARCADE);
+       integral.body.velocity.x = -90;
        timer.start();
-
+       var style = { font: "20px Kristen ITC ", fill: "#ffffff" };
+       pontos = game.add.text(550, 5, "Pontos: " + pontuacao,style);
 
    },
     update: function () {
+        let decrease = 90 * game.time.physicsElapsed;
+        fundo.tilePosition.x -= decrease;
 
-        fundo.tilePosition.x +=2;
-
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-        {
-            sprite1.y += 15;
-        }
-        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-        {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
             sprite1.y -= 15;
         }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            sprite1.y += 15;
+        }
+        
+        game.physics.arcade.collide(sprite1, mesa, jogadorPerde);
+        game.physics.arcade.overlap(sprite1,integral, function () {
+            ganhaPontos(sprite1, integral, pontos);
+        });
     },
 
     render: function () {
         if(timer.running){
-            game.debug.text(this.formatTime(Math.round((timerEvent.delay - timer.ms)/1000)),2,14,"#fdfbff");
+            game.debug.text(this.formatTime(Math.round((timerEvent.delay - timer.ms)/1000)),20,20,"#ffffff");
         }
         else{
-            game.debug.text("Tempo terminado!",2,14,"#EF180D");
+            game.debug.text("Tempo terminado!",20,20,"#EF180D");
         }
+
+
     },
     endTimer: function () {
         timer.stop();
@@ -67,8 +81,20 @@ let mainState = {
         let segundos = "0" + (s-minutos*60);
         return minutos.substr(-2)+":"+segundos.substr(-2);
     }
-   
+
+
 };
+function gerarMesas(){
+    
+}
+function jogadorPerde(mesa,aluno) {
+    mensagem = game.add.sprite(300,150,'mensagem');
+}
+function ganhaPontos(mesa,funcao, texto) {
+    pontuacao++;
+    texto.setText("Pontos: " + pontuacao);
+    funcao.kill();
+}
 
 game.state.add('mainState',mainState);
 game.state.start('mainState');
